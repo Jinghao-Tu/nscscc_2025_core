945030e6805e9f5a64a810ef5cbb6bccc7dbfef4
Jinghao Tu <jinghaotu213@jht213.com>
2025-03-18 16:14:21 +0800

    chore: init 2

--- hw/spinal/core/Config.scala
+++ hw/spinal/core/Config.scala
@@ -4,13 +4,13 @@ import spinal.core._
 import spinal.core.sim._
 
 object Config {
-  def spinal = SpinalConfig(
-    targetDirectory = "hw/gen",
-    defaultConfigForClockDomains = ClockDomainConfig(
-      resetActiveLevel = HIGH
-    ),
-    onlyStdLogicVectorAtTopLevelIo = true
-  )
+    def spinal = SpinalConfig(
+      targetDirectory = "hw/gen",
+      defaultConfigForClockDomains = ClockDomainConfig(
+        resetActiveLevel = HIGH
+      ),
+      onlyStdLogicVectorAtTopLevelIo = true
+    )
 
-  def sim = SimConfig.withConfig(spinal).withFstWave
+    def sim = SimConfig.withConfig(spinal).withFstWave
 }
